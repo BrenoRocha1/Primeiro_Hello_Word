b2645f4f001454200f5ebbe597f5e066241ca8a3
Breno Rocha <brenohenrique91901@gmail.com>
2024-02-19 09:38:46 -0300

    teste

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld{
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        //System.out é a saida padrão
+        //o pontinho é o operador de acesso a membro, oque está a direito do ponto é um membro 
+        //do que está a esquerda, out é membro de system
+    }
+}
+
+
